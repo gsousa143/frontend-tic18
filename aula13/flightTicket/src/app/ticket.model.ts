3ef6fa2462b87e1f299ec3b535a078fb83485473
gsousa143 <gabrielsousa4242@gmail.com>
2024-03-03 00:59:57 -0300

    organizando main

--- aula13/flightTicket/src/app/ticket.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Ticket {
-    NomePassageiro: string;
-    numeroVoo: string;
-    dataPartida: string;
-    dataChegada: string;
-    aeroportoPartida: string;
-    aeroportoChegada: string;
-    id?: string;
-}
